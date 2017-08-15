9991d7d869ee4d92bbaf053cd13e698654921170
Danilo Ricardo Dias Pena <danilord.pena@gmail.com>
2017-08-15 09:37:19 -0300

    Initial Commit

---
+++ dispatchAsync/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dispatchAsync
+//
+//  Created by Marcelo on 8/15/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
